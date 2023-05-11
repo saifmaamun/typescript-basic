02996808ddb78ee38c79eac78e27fafb43e10f7b
Saif Maamun <sadifmamun@gmail.com>
2023-05-11 18:05:41 +0600

    array

---
+++ src/array.ts
@@ -0,0 +1,8 @@
+// an declare arry in 2 ways
+const stringArray:string[] =['mishu', 'sakib', 'jalil'];
+const numberArray:Array<number> =[1,55,68,82]
+
+
+// can declare multiple data type
+const heroArray:(string|number)[] =['salman',52, 'akki']
+const userArray:Array<string|boolean|number> =['Ami', 28, 'nai', false]
