6b16bb2f5f5ee0c10429fbfece94e1346d61a06f
Saif Maamun <sadifmamun@gmail.com>
2023-05-11 17:39:32 +0600

    primitive data types

---
+++ src/data-types.ts
@@ -0,0 +1,23 @@
+// built in types
+
+
+let roll:number = 0;
+const firstName:string ='SAIF';
+const maritualStatus:boolean = false;
+
+const mentalStatus:undefined= undefined;
+// Undefined is the default value for uninitialized variables. which is not defined. 
+// Undefined means the variable has been declared, but its value has not been assigned.
+// Whenever we declare a variable without initializing it with a value, TypeScript initializes it as undefined . 
+
+
+const future:null= null;
+// The primitive type null indicates the intentional absence of a value. Therefore, in JavaScript, programmers often assign null to a variable to denote that the variable is declared and initialized and currently doesn’t hold a value. Still, a value can be expected in the future.
+
+
+
+
+
+
+
+// those are built in types
